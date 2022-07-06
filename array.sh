d08b4b6b7094c9383ce04d7edc25bb2138b75aa6
Katsiaryna Yakuts <95695292+katyayakuts@users.noreply.github.com>
2022-07-06 19:10:52 +0400

    test1

--- array.sh
+++ array.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+num=$1
+y=1
+
+for i in {a..z}; do
+ if [[ $y -le $num ]]
+         then
+                #echo $i
+                mkdir ./folder_$i
+                ((y++))
+ fi
+done
+
 # Place your code here
