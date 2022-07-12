0f4a2229b799f372ac94459c06e88af44693012c
Katsiaryna Yakuts <katyayakut@gmail.com>
2022-07-12 11:50:13 +0400

    test 7

--- array.sh
+++ array.sh
@@ -4,20 +4,17 @@ num=$1
 y=1
 
 for i in {a..z}; do
- if [[ $y -le $num ]]
-         then
-                #echo $i
-                mkdir ./folder_$i
-                ((y++))
+  if [[ $y -le $num ]]
+  then
+    #echo $i
+    mkdir ./folder_$i
+    ((y++))
  fi
 done
 
 if [[ $num -gt 1 ]]
 then
-        echo "$num folders created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')";
+  echo "$num folders created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')";
 else
-        echo "$num folder created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')"
+  echo "$num folder created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')"
 fi
-
-
-# Place your code here
